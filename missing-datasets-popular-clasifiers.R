d6b1c7dc20c88d7c720a2c2469d4a2bea5e64623
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2021-10-10 05:31:57 +0530

    Removed logistic activation

--- missing-datasets-popular-clasifiers.R
+++ missing-datasets-popular-clasifiers.R
@@ -5,100 +5,75 @@ for (k in 1:length(miss)) {
    
    data.renamed.labels <- labels.rename(data.miss.train[[k]], data.miss.test[[k]])
    
-   data.cleaned <- data.partition.multi(data.renamed.labels$TRAIN, 
-                                        data.renamed.labels$TEST)
+   result <- foreach(u = 1:100, 
+                     .combine = rbind,
+                     .packages = c('class','nnet')) %dopar% 
+   {
    
-   train.sample <- data.cleaned$TRAIN
-   test.sample <- data.cleaned$TEST
+      data.cleaned <- data.partition.multi(data.renamed.labels$TRAIN, 
+                                           data.renamed.labels$TEST)
+      
+      train.sample <- data.cleaned$TRAIN
+      test.sample <- data.cleaned$TEST
+      
+      train.label <- train.sample[,1]
+      test.label <- test.sample[,1]
+      
+      train.sample <- train.sample[,-1]
+      test.sample <- test.sample[,-1]
+      
+      N <- nrow(train.sample)
+      
+      ################################ Neural Networks
+      
+      Q <- data.frame(rbind(train.sample, test.sample))
+      targets <- class.ind(c(train.label, test.label))
+      
+      ##### ReLU Activation
    
-   train.label <- train.sample[,1]
-   test.label <- test.sample[,1]
+      mdl.nnet.ReLU.1 <- nnet(Q[1:N,], targets[1:N,], size = 1,
+                              decay = 5e-4, maxit = 100, MaxNWts = 500000,
+                              linout = TRUE)
    
-   train.sample <- train.sample[,-1]
-   test.sample <- test.sample[,-1]
+      mdl.nnet.ReLU.3 <- nnet(Q[1:N,], targets[1:N,], size = 3,
+                              decay = 5e-4, maxit = 100, MaxNWts = 500000,
+                              linout = TRUE)
    
-   N <- nrow(train.sample)
+      mdl.nnet.ReLU.5 <- nnet(Q[1:N,], targets[1:N,], size = 5,
+                              decay = 5e-4, maxit = 100, MaxNWts = 500000,
+                              linout = TRUE)
    
-   ################################ Neural Networks
+      mdl.nnet.ReLU.10 <- nnet(Q[1:N,], targets[1:N,], size = 10,
+                               decay = 5e-4, maxit = 100, MaxNWts = 500000,
+                               linout = TRUE)
    
-   ##### Logistic Activation
+      p_ReLU_1 <- predict(mdl.nnet.ReLU.1, Q[-c(1:N),])
+      p_ReLU_3 <- predict(mdl.nnet.ReLU.3, Q[-c(1:N),])
+      p_ReLU_5 <- predict(mdl.nnet.ReLU.5, Q[-c(1:N),])
+      p_ReLU_10 <- predict(mdl.nnet.ReLU.10, Q[-c(1:N),])
    
-   Q <- data.frame(rbind(train.sample, test.sample))
-   targets <- class.ind(c(train.label, test.label))
+      e_nnet_ReLU_1 <- mean(apply(p_ReLU_1, 1, which.max) != test.label)
+      e_nnet_ReLU_3 <- mean(apply(p_ReLU_3, 1, which.max) != test.label)
+      e_nnet_ReLU_5 <- mean(apply(p_ReLU_5, 1, which.max) != test.label)
+      e_nnet_ReLU_10 <- mean(apply(p_ReLU_10, 1, which.max) != test.label)
+      
+      
+      ################################ One Nearest Neighbour
+      
+      mdl.onn <- class::knn1(train = train.sample,
+                             test = test.sample,
+                             cl = as.factor(train.label))
+      
+      e_onn <- mean(mdl.onn != test.label)
+      
+      
+      return(c(e_nnet_ReLU_1, e_nnet_ReLU_3, e_nnet_ReLU_5, e_nnet_ReLU_10,
+               e_onn))
    
-   print("Training Neural Network started...")
+   }
    
-   mdl.nnet.logistic.1 <- nnet(Q[1:N,], targets[1:N,], size = 1,
-                               decay = 5e-4, maxit = 100, MaxNWts = 10100,
-                               linout = FALSE)
+   path.current <- paste0("E:\\Jyotishka\\Code\\",miss[k],"_NN-ONN.xlsx")
    
-   print("Hidden layer = 1 DONE")
-   
-   mdl.nnet.logistic.3 <- nnet(Q[1:N,], targets[1:N,], size = 3,
-                               decay = 5e-4, maxit = 100, MaxNWts = 10100,
-                               linout = FALSE)
-   
-   print("Hidden layer = 3 DONE")
-   
-   mdl.nnet.logistic.5 <- nnet(Q[1:N,], targets[1:N,], size = 5,
-                               decay = 5e-4, maxit = 100, MaxNWts = 10100,
-                               linout = FALSE)
-   
-   print("Hidden layer = 5 DONE")
-   
-   mdl.nnet.logistic.10 <- nnet(Q[1:N,], targets[1:N,], size = 10,
-                                decay = 5e-4, maxit = 100, MaxNWts = 10100,
-                                linout = FALSE)
-   
-   print("Hidden layer = 10 DONE")
-   
-   p_log_1 <- predict(mdl.nnet.logistic.1, Q[-c(1:N),])
-   p_log_3 <- predict(mdl.nnet.logistic.3, Q[-c(1:N),])
-   p_log_5 <- predict(mdl.nnet.logistic.5, Q[-c(1:N),])
-   p_log_10 <- predict(mdl.nnet.logistic.10, Q[-c(1:N),])
-   
-   e_nnet_log_1 <- mean(apply(p_log_1, 1, which.max) != test.label)
-   e_nnet_log_3 <- mean(apply(p_log_3, 1, which.max) != test.label)
-   e_nnet_log_5 <- mean(apply(p_log_5, 1, which.max) != test.label)
-   e_nnet_log_10 <- mean(apply(p_log_10, 1, which.max) != test.label)
-   
-   
-   # ##### ReLU Activation
-   
-   # mdl.nnet.ReLU.1 <- nnet(Q[1:40,], targets[1:N,], size = 1,
-   #                         decay = 5e-4, maxit = 100, MaxNWts = 10100,
-   #                         linout = TRUE)
-   # 
-   # mdl.nnet.ReLU.3 <- nnet(Q[1:40,], targets[1:N,], size = 3,
-   #                         decay = 5e-4, maxit = 100, MaxNWts = 10100,
-   #                         linout = TRUE)
-   # 
-   # mdl.nnet.ReLU.5 <- nnet(Q[1:40,], targets[1:N,], size = 5,
-   #                         decay = 5e-4, maxit = 100, MaxNWts = 10100,
-   #                         linout = TRUE)
-   # 
-   # mdl.nnet.ReLU.10 <- nnet(Q[1:40,], targets[1:N,], size = 10,
-   #                          decay = 5e-4, maxit = 100, MaxNWts = 10100,
-   #                          linout = TRUE)
-   # 
-   # p_ReLU_1 <- predict(mdl.nnet.ReLU.1, Q[-c(1:N),])
-   # p_ReLU_3 <- predict(mdl.nnet.ReLU.3, Q[-c(1:N),])
-   # p_ReLU_5 <- predict(mdl.nnet.ReLU.5, Q[-c(1:N),])
-   # p_ReLU_10 <- predict(mdl.nnet.ReLU.10, Q[-c(1:N),])
-   # 
-   # e_nnet_ReLU_1 <- mean(apply(p_ReLU_1, 1, which.max) != test.label)
-   # e_nnet_ReLU_3 <- mean(apply(p_ReLU_3, 1, which.max) != test.label)
-   # e_nnet_ReLU_5 <- mean(apply(p_ReLU_5, 1, which.max) != test.label)
-   # e_nnet_ReLU_10 <- mean(apply(p_ReLU_10, 1, which.max) != test.label)
-   
-   
-   ################################ One Nearest Neighbour
-   
-   mdl.onn <- class::knn1(train = train.sample,
-                          test = test.sample,
-                          cl = as.factor(train.label))
-   
-   e_onn <- mean(mdl.onn != test.label)
-   
+   writexl::write_xlsx(result, path = path.current)
    
 }
