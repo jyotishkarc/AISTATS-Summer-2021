4dacd35c38e69be9ed00346cb92c74c927287517
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2021-10-11 23:37:55 +0530

    Major edits

--- Tabulation-New.R
+++ Tabulation-New.R
@@ -18,19 +18,14 @@ N <- nrow(all.UCR)
 M.UCR.1 <- read.csv("D:/My Documents/Real Data Analysis_ Three Databases - UCR.csv")
 # M.orig <- read.csv("E:/Jyotishka/Real Data Analysis_ Three Databases - UCR.csv")
 
-
 # df.table <- as.data.frame(matrix(NA, nrow = N, ncol = 15))
 df.table <- M.UCR.1
-
 
 colnames(df.table) <- H <- c("Dataset", "Length", 
                              "delta0.sin","delta0.sin.comp",
                              "delta2.sin", "delta2.sin.comp", 
                              "GLMNET", "RF", "RP","SVMlin", "SVMRBF",
                              "Nnet", "1NN", "SAVG")
-
-# mV.which <- which(str_detect(files, "majorityVoting"))
-# proj.which <- which(str_detect(files, "projavg"))
 
 Z <- list()
 
@@ -50,21 +45,40 @@ for (k in 1:N) {
          popular.mean <- colMeans(temp.popular)
          
          # RF.pos <- which.min(popular.mean[5:8]) + 4
-         popular.mean <- c(popular.mean[1:4], min(popular.mean[5:8]))
+         popular.mean <- c(popular.mean[1:4], "RF" = min(popular.mean[5:8]))
          
          #popular.se <- apply(temp.popular, 2, function(val) sciplot::se(val))
          #popular.se <- c(popular.se[1:4], popular.se[RF.pos])
          
          popular.mean <- c(popular.mean[1], popular.mean[5], popular.mean[2],
-                           popular.mean[3], M.UCR.1$SVMRBF[k])
+                           popular.mean[3], "SVMRBF" = M.UCR.1$SVMRBF[k])
          # popular.se <- c(popular.se[1], popular.se[5], popular.se[2],
          #                 popular.se[3], popular.se[4], NA, NA)
-      }
-      else {
-         popular.mean <- c(rep(NA,4), M.UCR.1$SVMRBF[k])
-      }
       
-      df.table[k, c(7:11)] <- popular.mean
+         df.table[k, c(7:11)] <- popular.mean
+      }
+      # else {
+      #    popular.mean <- c(rep(NA,4), M.UCR.1$SVMRBF[k])
+      # }
+   }
+   
+   
+   if (is.na(as.numeric(M.UCR.1[k,3])) == FALSE && 
+       is.na(as.numeric(M.UCR.1[k,12])) == TRUE) {
+   
+      nn.onn <- Z[[k]] %>% filter(str_detect(files, "NN-ONN"))
+      
+      if (prod(dim(nn.onn)) != 0) {
+         temp.nn.onn <- read_xlsx(paste(path, as.character(nn.onn), sep = ""))
+         temp.nn.onn <- as.matrix(temp.nn.onn)
+         nn.onn.mean <- colMeans(temp.nn.onn)
+         nn.onn.mean <- c("Nnet" = min(nn.onn.mean[1:4]), nn.onn.mean[5])
+         
+         df.table[k, c(12:13)] <- nn.onn.mean
+      }
+      # else {
+      #    nn.onn.mean <- rep(NA, 2)
+      # }
    }
 }
 
@@ -77,26 +91,44 @@ View(df.table)
 
 
 pref.mat <- matrix(0, N, 10)
+pref.mat.RF <- matrix(0, N, 11)
+
 B <- H[c(3:4,6:7,9:14)]
+C <- H[c(3:4,6:14)]
 
 for (i in 1:N) {
    if(is.na(sum(df.table[i,c(3:4,6:7,9:14)])) == FALSE){
       R <- df.table[i,c(3:4,6:7,9:14)] %>%
          as.numeric() %>%
          rank(ties.method = "first")
+      
+      S <- df.table[i,c(3:4,6:14)] %>%
+         as.numeric() %>%
+         rank(ties.method = "first")
 
-      v <- c()
+      v <- h <- c()
       for(j in 1:10){
          v[j] <- B[which(R == j)]
       }
-
+      
+      for(j in 1:11){
+         h[j] <- C[which(S == j)]
+      }
+      
       pref.mat[i,] <- v
+      pref.mat.RF[i,] <- h
    }
-   else pref.mat[i,] <- rep(NA,10)
+   else{
+      pref.mat[i,] <- rep(NA, 10)
+      pref.mat.RF[i,] <- rep(NA,11)
+   }
 }
 
 pref.mat <- cbind(all.UCR$datasets, pref.mat)
+pref.mat.RF <- cbind(all.UCR$datasets, pref.mat.RF)
+
 pref.mat.no.NA <- pref.mat %>% na.omit() %>% as.data.frame()
+pref.mat.RF.no.NA <- pref.mat.RF %>% na.omit() %>% as.data.frame()
 
 View(pref.mat.no.NA)
 
